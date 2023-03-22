42f80577f05e497e22e7646de9be2350fb189158
Justin Anzalone <justin.m.anzalone@gmail.com>
2023-03-22 12:38:23 -0700

    homework.js complete

---
+++ lectures/week02/3-Wednesday/MY-LectureNotes/hw-js101-dataTypes/homework.js
@@ -0,0 +1,57 @@
+// Problem 1
+let firstName = "Justin ";
+let lastName = "Anzalone";
+let name = firstName + lastName;
+console.log(name);
+
+// Problem 2
+let a = 2;
+let b = 3;
+let x = a+b;
+console.log(x);
+
+// Problem 3
+let x1 = "3"
+let y1 = "15"
+let result = parseInt(x1)+parseInt(y1);
+console.log(result);
+
+// Problem 4
+
+let num1 = 12;
+let num2 = 14;
+let product = num1*num2;
+console.log(`The product of ${num1} and ${num2} is ${product}`);
+
+// Problem 5
+let num = 5;
+let square = Math.pow(5,2)
+console.log(`The square of ${num} is ${square}`);
+
+// Problem 6
+let numm1 = 10;
+let numm2 = 5;
+let difference = numm1-numm2;
+console.log(`The difference between ${numm1} and ${numm2} is ${difference}`);
+
+// Problem 7
+let num3 = 100;
+let num4 = 10;
+let quotient = num3/num4;
+console.log(`The quotion of ${num3} and ${num4} is ${quotient}`)
+
+// Problem 8
+let d = -234;
+let e = -89;
+let f = Math.abs(d+e);
+console.log(f);
+
+// Problem 9
+let pow = 6;
+let result6 = Math.pow(pow,7)
+console.log(result6);
+
+// Problem 10
+const pi = 3.14;
+result = Math.round(pi)
+console.log(result);
